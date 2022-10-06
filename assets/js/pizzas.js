e0bd0a14a73da593f69b59c636431b8b47191963
eduardo <eduardobatera2008@hotmail.com>
2022-10-05 22:27:57 -0300

    Pizzaria

---
+++ assets/js/pizzas.js
@@ -0,0 +1,58 @@
+let pizzaJson = [
+    {
+        id:1, 
+        name:'Mussarela', 
+        img:'assets/img/pizza.png',
+        price:[25.00, 35.00, 45.00], 
+        sizes:['35 cm', '45 cm', '60 cm'], 
+        description:'Queijo de mussarela, Molho de tomate, Tomate em rodelas e Orégano a gosto'
+    },
+    {
+        id:2, 
+        name:'Calabresa', 
+        img:'assets/img/pizza2.png', 
+        price:[25.00, 35.00, 45.00], 
+        sizes:['35 cm', '45 cm', '60 cm'], 
+        description:'Linguiça calabresa defumada, sem casca e cebolas em rodelas finas'
+    },
+    {
+        id:3, 
+        name:'Quatro Queijos', 
+        img:'assets/img/pizza3.png', 
+        price:[25.00, 35.00, 45.00], 
+        sizes:['35 cm', '45 cm', '60 cm'], 
+        description:'Queijos: mussarela, gorgonzola, parmesão e provolone'
+    },
+    {
+        id:4, 
+        name:'Americana', 
+        img:'assets/img/pizza4.png', 
+        price:[25.00, 35.00, 45.00], 
+        sizes:['35 cm', '45 cm', '60 cm'], 
+        description:'Mussarela ralada, pepperoni fatiado fino e folhas de orégano fresco para decorar'
+    },
+    {
+        id:5, 
+        name:'Sorvete', 
+        img:'assets/img/pizza5.png', 
+        price:[25.00, 35.00, 45.00], 
+        sizes:['35 cm', '45 cm', '60 cm'], 
+        description:'Massa de pão de ló, sorvete de creme, chocolate branco em fatias, cobertura para sorvetes, castanha de caju picadas'
+    },
+    {
+        id:6, 
+        name:'Moda da Casa', 
+        img:'assets/img/pizza6.png', 
+        price:[25.00, 35.00, 45.00], 
+        sizes:['35 cm', '45 cm', '60 cm'], 
+        description:'Ovos cozidos, cebola, queijo prato presunto e orégano'
+    },
+    {
+        id:7, 
+        name:'Chocolate com M&M´s', 
+        img:'assets/img/pizza7.png', 
+        price:[25.00, 35.00, 45.00], 
+        sizes:['35 cm', '45 cm', '60 cm'], 
+        description:'Chocolate ao leite e grãos de M&M´s'
+    }
+];
